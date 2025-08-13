af530d33de0ab061a9fdb650a3ed33495f5fe165
pamonha15 <eduardo.viana.oliveira@escola.pr.gov.br>
2025-08-13 20:52:29 -0300

    Create script.js aulan 8

---
+++ script.js
@@ -0,0 +1,9 @@
+window.sr = ScrollReveal({ reset: true }};
+
+sr.reavel("image" , {duretion;1000});
+
+sr.raevel("#textoconteudo" , {
+  rotate; {x=0,y=100,z=0}),
+  duration:1500}):
+
+sr.reavel("#formulario", {duratin:1000});
